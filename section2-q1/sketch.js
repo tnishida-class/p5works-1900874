910c7c473d5b2ab303f1090cfac008c76b2f1ae8
1900874 <72754697+1900874@users.noreply.github.com>
2020-10-24 23:39:14 +0900

    2-Q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,12 @@
 // 小手調べ
-function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+function setup() {
+createCanvas(400,400);
+stroke(255,0,0)
+for(let x=200;x>=100;x=x-20) {
+ellipse(200,200,x);
+}
+stroke(0,0,255);
+for(let x=100;x>=0;x=x-20){
+ellipse(200,200,x)
+}
 }
