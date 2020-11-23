60f29b9dc9ff0441b66287f5e872d7e07c38e1de
1900874 <72754697+1900874@users.noreply.github.com>
2020-11-24 01:50:48 +0900

    2-Q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -32,3 +32,21 @@ stroke(0);
  ellipse(x,325,45);
  }
 }
+if(i % 2 == 0 && j % 2 == 0){
+        fill(255)}
+      else if(i % 2 == 1 && j % 2 == 1){
+        fill(255)}
+      else{fill(128)}
+      rect(i * size, j * size, 30, 30);
+
+if(i % 2 == 1 &&j % 2 == 0 && j < 3){
+        fill(255,0,0)}
+      else if (i % 2 == 0 && j % 2 == 1 && j < 3){
+        fill(255,0,0)}
+      else if(i % 2 == 1 && j % 2 == 0 && j > 4){
+        fill(0)}
+      else if(i % 2 == 0 && j % 2 == 1 && j > 4){
+        fill(0)}
+  ellipse(i * size + 12.5,j * size + 12.5,20)
+      }
+        }}
